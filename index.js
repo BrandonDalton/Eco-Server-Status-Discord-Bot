55bcd2c79b4283181bdcb51b6caf0c1e0c2c4bee
Brandon D <42697737+BrandonDalton@users.noreply.github.com>
2021-09-07 23:41:04 +1000

    Add Whos Online Command

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, token_dev } = require('./config.json');
 const axios = require('axios');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING] });
 const playerCountAPI = "http://172.105.168.24:3001/info"
 
 // When the client is ready, run this code (only once)
@@ -20,7 +20,25 @@ client.once('ready', () => {
         });
     }, 300 * 1000)
 });
+
+//Whos Online Command
+client.on('message', (msg) => {
+    if (msg.content === "!whosonline") {
+        axios.get(playerCountAPI)
+            .then(response => {
+                const playersArr = response.data.OnlinePlayersNames
+                const playersString = playersArr.join(', ');
+                msg.reply(playersString + " are playing!");
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
+});
+
 client.login(token);
 // Login to Discord with your client's token
+
 
 
